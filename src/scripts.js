ed37a66c438446d1451f7f93832ed3f030c9d700
FixesMix <shiseui.00@gmail.com>
2025-01-17 12:47:18 -0500

    Updated card

---
+++ src/scripts.js
@@ -0,0 +1,57 @@
+//fade in Main Container on startup.JS
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".fade-in");
+  container.classList.add("show");
+});
+
+
+//For working tooltips
+document.addEventListener("DOMContentLoaded", function () {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
+
+
+//For mallow feeties
+document.addEventListener("DOMContentLoaded", () => {
+  const footerImage = document.querySelector(".footer-image");
+  const FeetiesContainer = document.querySelector(".sliding-footer");
+  const maxFeeties = 100;
+  const Feeties = [];
+  let feetiesIndex = 0; //Footprint count
+
+
+
+  // Establish object pooling
+  for (let i = 0; i < maxFeeties; i++) {
+    const feeties = document.createElement("div");
+    feeties.classList.add("feeties");
+    feeties.style.display = "none"; //At first, all footprints are hidden
+    FeetiesContainer.appendChild(feeties);
+    Feeties.push(feeties);
+  }
+
+
+  //Establish footprint interval
+  const interval = 500;
+  setInterval(() => {
+    const characterRight = parseFloat(
+      getComputedStyle(footerImage).right.replace("px", "")
+    );
+
+
+
+    //If the character is visible, footprints may be placed.
+    if (characterRight >= -50 && characterRight <= FeetiesContainer.offsetWidth) {
+      const feeties = Feeties[feetiesIndex];
+      feeties.style.display = "block"; // make visible
+      feeties.style.right = `${characterRight}px`; // Update print position
+      feeties.style.bottom = "10%"; // prints are always near bottom of footer
+      feetiesIndex = (feetiesIndex + 1) % maxFeeties; // Cycling through pool
+    }
+  }, interval);
+});
+//end of mallow feeties
